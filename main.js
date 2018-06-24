5f5c07582d6f64ed12953853d15622d33c9bc78d
Iván Sánchez <34893604+IvanSGurdian@users.noreply.github.com>
2018-06-24 17:40:44 -0600

    Basic functionality complete

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $(".toggleBtn").click(function () {
     $(this).toggleClass('active');
+
+    var panelId = $(this).attr("id") + "Panel";
+
+    $("#" + panelId).toggleClass('hidden');
+
+    var activePanels = 4 - $('.hidden').length;
+    console.log(activePanels)
+
+    $('.panel').width(($(window).width() / activePanels) - 21)
+
 })
 
-$('.panel').height($(window).height() - $('header').height())
+function updateOutput(){
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $('#cssPanel').val() + "</style></head><body>" + $('#htmlPanel').val() + "</body></html>");
+
+    document.getElementById("outputPanel").contentWindow.eval($('#javascriptPanel').val());
+}
+
+$('.panel').height($(window).height() - $('header').height());
+
+updateOutput();
+
+$('textarea').on('change keyup paste', function () {
+    updateOutput();
+})
